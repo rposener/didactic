8295faa5ad1a47d73e4b460fd96d254871f69bd1
Ryan Posener <ryan.posener@agilethought.com>
2020-01-21 13:05:07 -0700

    calculator working decent

--- src/components/calculator.ts
+++ src/components/calculator.ts
@@ -2,11 +2,15 @@ import { DidactComponentBase, Observable } from "../didact/componentbase";
 import html from "./calculator.html";
 
 class CalculatorComponent extends DidactComponentBase {
-    currentNumber: Observable<string>;
+    calcEquation: Observable<string>;
+    calcEntry: Observable<string>;
+    clearEquationOnNext: boolean;
 
     constructor() {
         super("calculator-template", html);
-        this.currentNumber = new Observable<string>("0");
+        this.calcEquation = new Observable<string>("");
+        this.calcEntry = new Observable<string>("0");
+        this.clearEquationOnNext = true;
     }
 
     connectedCallback(): void {
@@ -21,12 +25,68 @@ class CalculatorComponent extends DidactComponentBase {
         buttonArea.removeEventListener("click", this.buttonClick);
     }
 
-    buttonClick = (ev:Event): void => {
+    buttonClick = (ev: Event): void => {
+        if (this.clearEquationOnNext) {
+            this.calcEquation.value = "";
+            this.clearEquationOnNext = false;
+        }
         const btn = ev.target as HTMLButtonElement;
         if (btn.classList.contains("calc-number")) {
-            let value = parseInt(this.currentNumber.value + btn.value);
-            this.currentNumber.value = value.toString();
+            let value = parseFloat(this.calcEntry.value + btn.value);
+            this.calcEntry.value = value.toString();
+        } else if (btn.classList.contains("calc-negate")) {
+            let value = parseFloat(this.calcEntry.value) * -1;
+            this.calcEntry.value = value.toString();
+        } else if (btn.classList.contains("calc-decimal")) {
+            this.calcEntry.value = this.calcEntry.value + ".";
+        } else if (btn.classList.contains("calc-divide")) {
+            let value = parseFloat(this.calcEntry.value);
+            this.calcEquation.value = this.calcEquation.value + " " + value + " /";
+            this.calcEntry.value = "0";
+        } else if (btn.classList.contains("calc-multiply")) {
+            let value = parseFloat(this.calcEntry.value);
+            this.calcEquation.value = this.calcEquation.value + " " + value + " *";
+            this.calcEntry.value = "0";
+        } else if (btn.classList.contains("calc-minus")) {
+            let value = parseFloat(this.calcEntry.value);
+            this.calcEquation.value = this.calcEquation.value + " " + value + " -";
+            this.calcEntry.value = "0";
+        } else if (btn.classList.contains("calc-plus")) {
+            let value = parseFloat(this.calcEntry.value);
+            this.calcEquation.value = this.calcEquation.value + " " + value + " +";
+            this.calcEntry.value = "0";
+        } else if (btn.classList.contains("calc-execute")) {
+            let value = parseFloat(this.calcEntry.value);
+            const equation = this.calcEquation.value + " " + value;
+            const ttl = this.evaluateEquation(equation);
+            this.calcEquation.value = equation + " = " + ttl;
+            this.calcEntry.value = "0";
+            this.clearEquationOnNext = true;
         }
+    }
+
+    private evaluateEquation(equation: string): number {
+        const eqParts = equation.trim().split(' ');
+        let ttl = parseFloat(eqParts[0]);
+        for (let x = 1; x < eqParts.length; x += 2) {
+            const val = parseFloat(eqParts[x + 1]);
+            const op = eqParts[x];
+            switch (op) {
+                case "+":
+                    ttl += val;
+                    break;
+                case "-":
+                    ttl -= val;
+                    break;
+                case "*":
+                    ttl *= val;
+                    break;
+                case "/":
+                    ttl /= val;
+                    break;
+            }
+        }
+        return ttl;
     }
 }
 
